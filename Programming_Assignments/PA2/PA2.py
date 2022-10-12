5c78c0989d8c98427c35f2940dbdf0bbe92ab02b
Amethyst Skye <amethystskye.pi@gmail.com>
2022-10-12 14:41:16 -0700

    Tried df.corr()

--- Programming_Assignments/PA2/PA2.py
+++ Programming_Assignments/PA2/PA2.py
@@ -1,45 +1,44 @@
 import pandas as pd
+import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-movies = pd.read_csv('movie-lens-data/movies.csv')
+#movies = pd.read_csv('movie-lens-data/movies.csv')
 #links = pd.read_csv('movie-lens-data/links.csv')
 ratings = pd.read_csv('movie-lens-data/ratings.csv')
 #tags = pd.read_csv('movie-lens-data/tags.csv')
 
 # Matrix containing all ratings per user per movie
-movie_matrix = ratings.pivot_table(index = 'movieId', columns = 'userId', values = 'rating') # shape = (9066, 671)
+'''movie_matrix = ratings.pivot_table(index = 'movieId', columns = 'userId', values = 'rating') # shape = (9066, 671)
 # Normalize matrix
-matrix_norm = movie_matrix.subtract(movie_matrix.mean(axis = 1), axis = 0) # shape = (9066, 671) (row, col)
+matrix_norm = movie_matrix.subtract(movie_matrix.mean(axis = 0), axis = 1) # shape = (9066, 671) (row, col)
 # Cosine Similarity (movie to movie)
-cos_matrix = cosine_similarity(matrix_norm.fillna(0)) # shape = (9066, 9066)
-movie_matrix = ratings.pivot_table(index = 'userId', columns = 'movieId', values = 'rating') # shape = (9066, 671)
+corr_matrix = pd.DataFrame(cosine_similarity(matrix_norm.fillna(0))) # shape = (9066, 9066)
+# Set correlation matrix columns and indexes as 'movieId'
+movieIds = pd.Series(movie_matrix.index)
+corr_matrix.set_index([movieIds.values], inplace=True)
+corr_matrix.columns = [movieIds.values]'''
 
-# List containing each movieId and corresponding index
-movie_list = []
-for x in movie_matrix.columns:
-    movie_list.append(x)
+movie_matrix = ratings.pivot_table(index = 'userId', columns = 'movieId', values = 'rating') # shape = (9066, 671)
+corr2_matrix = movie_matrix.corr(method = 'pearson')
 
 # Looping through each user (i)
-for i in range(1,2): #len(movie_matrix)
-    user_ratings = movie_matrix.iloc[i - 1].dropna() # gives us row for ith user
+for user in range(1,10): #len(movie_matrix)
+    user_ratings = movie_matrix.iloc[user - 1].dropna() # gives us row for ith user
     recommend = pd.Series([],dtype=pd.StringDtype()) 
     temp = []
     
-    # Looping through each movie (j) for each user (i)
-    for j in range(0, len(user_ratings)):
-        # extract movie from similarity matrix
-        # user_ratings[j] gives us the movieId of a rated movie
-        print(user_ratings.index[j])
-        print(cos_matrix[user_ratings.index[j]])
-        similar = pd.Series(cos_matrix[user_ratings.index[j]]).dropna() 
-        similar = similar.map(lambda x: x * user_ratings.values[j])
+    # Looping through each movie for each user
+    for movie in range(0, len(user_ratings)):
+        similar = pd.Series(corr2_matrix[user_ratings.index[movie]]).dropna() 
+        similar = similar.map(lambda x: x * user_ratings.values[movie])
         temp.append(similar)
         recommend = pd.concat(temp)
-        
+
     recommend.sort_values(inplace = True, ascending = False)
     x = pd.DataFrame(recommend)
     recommend_filter = x[~x.index.isin(user_ratings.index)]
-    print(i, recommend_filter.index[0], 
+    #print(user, recommend.head(5))
+    print(user, recommend_filter.index[0], 
           recommend_filter.index[1],
           recommend_filter.index[2],
           recommend_filter.index[3],
